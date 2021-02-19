5bc3462f989dc7d787586f0e328217b06a3df518
Tammana <tammana1400.cse18@chitkara.edu.in>
2021-02-20 05:24:19 +0530

    Initial commit

---
+++ app/src/main/java/com/example/mynotes/NotesDao.kt
@@ -0,0 +1,17 @@
+package com.example.mynotes
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+
+@Dao
+interface NotesDao {
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    //suspend to run these processes in background threads it is called coroutines.
+    suspend fun insert(note:Notes)
+    @Delete
+    suspend fun delete(note: Notes)
+    @Query("Select * from notes_table order by id ASC")
+    //with the help of live data our observer in our case activity knows when there is changes occur database
+
+    fun getAllNotes(): LiveData<List<Notes>>
+}
---
+++ app/src/main/java/com/example/mynotes/NotesRepository.kt
@@ -0,0 +1,16 @@
+package com.example.mynotes
+
+import androidx.lifecycle.LiveData
+
+class NotesRepository(private val notesDao:NotesDao) {
+    val allNotes: LiveData<List<Notes>> = notesDao.getAllNotes()
+    suspend fun insert(note:Notes)
+    {
+        notesDao.insert(note)
+    }
+    suspend fun delete(note:Notes)
+    {
+        notesDao.delete(note)
+    }
+    
+}
